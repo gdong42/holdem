ea3b5b79baa6b7961c374ef5d09e0f6442931aa6
Gan Dong <donggan@xinzhili.cn>
2017-02-11 14:50:18 +0800

    Set up interative shell ui

---
+++ src/main/java/info/donggan/ui/cli/HoldemBannerProvider.java
@@ -0,0 +1,38 @@
+package info.donggan.ui.cli;
+
+import org.springframework.shell.plugin.BannerProvider;
+import org.springframework.stereotype.Component;
+
+/**
+ * Date: 11/02/2017
+ * Time: 2:05 PM
+ *
+ * @author Gan Dong
+ */
+@Component
+public class HoldemBannerProvider implements BannerProvider {
+  @Override
+  public String getBanner() {
+    return "  __                                 .__           .__       .___              \n" +
+        "_/  |_  ____ ___  ________    ______ |  |__   ____ |  |    __| _/____   _____  \n" +
+        "\\   __\\/ __ \\\\  \\/  /\\__  \\  /  ___/ |  |  \\ /  _ \\|  |   / __ |/ __ \\ /     \\ \n" +
+        " |  | \\  ___/ >    <  / __ \\_\\___ \\  |   Y  (  <_> )  |__/ /_/ \\  ___/|  Y Y  \\\n" +
+        " |__|  \\___  >__/\\_ \\(____  /____  > |___|  /\\____/|____/\\____ |\\___  >__|_|  /\n" +
+        "           \\/      \\/     \\/     \\/       \\/                  \\/    \\/      \\/ ";
+  }
+
+  @Override
+  public String getVersion() {
+    return "0.0.1a";
+  }
+
+  @Override
+  public String getWelcomeMessage() {
+    return "Welcome! Good luck";
+  }
+
+  @Override
+  public String getProviderName() {
+    return "Holdem Banner";
+  }
+}
